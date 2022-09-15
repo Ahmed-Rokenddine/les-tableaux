18e515ebfbf87b0ce3de4b767249b95e2374daeb
Ahmed-Rokenddine <masteriko005@gmail.com>
2022-09-15 14:02:37 +0100

    recherche ajoute sur tri selective

---
+++ excercice avec tab+tri+recherche.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+ int main(){
+    int i,j,n,x;
+
+      printf("entrer le nombre de cases que vous voulez:"); //la taille du tableau -- Size of the table
+      scanf("%d",&n);
+
+      int tab[n];
+       for ( i = 0; i < n ; i++) 
+       {
+        printf("l'element %d est :",i+1); //remplissage du tableau - filling the table
+        scanf("%d",&tab[i]);
+       }
+
+
+
+       for ( i = 0; i < n -1; i++)      //tri par selection
+       {
+           x=i;                         // initaliser l'index de x par i 
+           
+           
+           
+           for(j=i+1;j < n ; j++){      // boucle autour des element du tableau
+             
+             if (tab[x]>tab[j]){                   // si un element a une valeur plus petite que la valeau de  x  alors echange leur index ;
+                 x=j;
+             }
+       }
+        if (x!=i){ // si la condition cite avant est realise , alors echange leur valeur
+            int tmp=tab[i];
+            tab[i]=tab[x];
+            tab[x]=tmp;
+        }
+       }
+       // une boucle qui nous affiche le tableau apres son tri en ordre
+    for  ( i = 0; i < n ; i++) {
+        printf("%d\n",tab[i]);
+    }
+    
+    int r;
+     printf("saisisser votre recherche :");
+     scanf("%d",&r);
+
+    int start=0;
+    int end=n;
+    int mid=(end+start)/2;
+    while(tab[mid]!=r && start <= end){
+      if (tab[mid]>r)
+         {
+         end=mid-1;
+         mid=(start+end)/2;
+         }
+     else 
+       start=mid+1;
+       mid=(start+end)/2;
+    }
+   if (tab[mid]==r)
+   {
+    printf("l'element se trouve parmi les resultat");
+   }
+    else printf("l'element est introuvable"); 
+   
+   return 0;     
+ }
+ 
+ // fin du programme
--- tableaux .c
+++ tableaux .c
@@ -68,6 +68,49 @@ int max,k, l,v[5];
                 se base sur la comparaison des donnes pour arriver au resultat
                              Tableau croissant
 */
+/*
+
+
+  int main(){
+    int i,j,n,x;
+
+      printf("entrer le nombre de cases que vous voulez:"); //la taille du tableau -- Size of the table
+      scanf("%d",&n);
+
+      int tab[n];
+       for ( i = 0; i < n ; i++) 
+       {
+        printf("l'element %d est :",i+1); //remplissage du tableau - filling the table
+        scanf("%d",&tab[i]);
+       }
+
+
+
+       for ( i = 0; i < n -1; i++)      //tri par selection
+       {
+           x=i;                         // initaliser l'index de x par i 
+           
+           
+           
+           for(j=i+1;j < n ; j++){      // boucle autour des element du tableau
+             
+             if (tab[x]<tab[j]){                   // si un element a une valeur plus petite que la valeau de  x  alors echange leur index ;
+                 x=j;
+             }
+       }
+        if (x!=i){ // si la condition cite avant est realise , alors echange leur valeur
+            int tmp=tab[i];
+            tab[i]=tab[x];
+            tab[x]=tmp;
+        }
+       }
+       // une boucle qui nous affiche le tableau apres son tri en ordre
+    for  ( i = 0; i < n ; i++) {
+        printf("%d\n",tab[i]);
+    } 
+    */
+
+
   int Y[10];
   int w, u, r, x;
  
@@ -108,6 +151,8 @@ int max,k, l,v[5];
    //afficher les éléments du tableau
    printf("les elemetes du tableau sont :\n");
    for (w=0; w < 10; ++w)
+
+
    {
       scanf("%d",&Y[w]);
    }
@@ -196,64 +241,74 @@ int max,k, l,v[5];
     
  }
                                                                     recherche par dichotomique
-     int main(){
-    int i,tmp,x,start,end,mid,a;
-     printf("combien de case voulez vous ?:\n");
-     scanf("%d",&x);
-     int f[x];
-     printf("Donner la totalite des nombres:\n");
-      for ( i = 0; i <x ; i++)
-      {
-        scanf("%d",&f[i]);
-     
-     }
-     printf("le tri en ordre croissant sera:\n");
-     
-    for ( int j = 0; j <=x-1 ; j++)
-    {
-        while (f[j]>0 && f[j-1]>f[j])
-        {
-         tmp=f[j];
-         f[j]=f[j-1];
-         f[j-1]=tmp;
+    #include <stdio.h>
+#include <math.h>
+#include <string.h>
+ int main(){
+    int i,j,n,x;
 
-         j--;
+      printf("entrer le nombre de cases que vous voulez:"); //la taille du tableau -- Size of the table
+      scanf("%d",&n);
+
+      int tab[n];
+       for ( i = 0; i < n ; i++) 
+       {
+        printf("l'element %d est :",i+1); //remplissage du tableau - filling the table
+        scanf("%d",&tab[i]);
+       }
+
+
+
+       for ( i = 0; i < n -1; i++)      //tri par selection
+       {
+           x=i;                         // initaliser l'index de x par i 
+           
+           
+           
+           for(j=i+1;j < n ; j++){      // boucle autour des element du tableau
+             
+             if (tab[x]>tab[j]){                   // si un element a une valeur plus petite que la valeau de  x  alors echange leur index ;
+                 x=j;
+             }
+       }
+        if (x!=i){ // si la condition cite avant est realise , alors echange leur valeur
+            int tmp=tab[i];
+            tab[i]=tab[x];
+            tab[x]=tmp;
         }
+       }
+       // une boucle qui nous affiche le tableau apres son tri en ordre
+    for  ( i = 0; i < n ; i++) {
+        printf("%d\n",tab[i]);
     }
-    for ( int k = 0; k < x ; k++)
-    {
-      printf("%d\n",f[k]);
-    }
-     printf("saisisser votre recherche :");
-     scanf("%d",&a);
- start=0;
- end=x-1;
- mid=(end-start)/2;
-        while  (f[mid]!=a )
-   {
-      if (f[mid]>a)
-      {
-         end=mid-1;
-      }
-    else
-       start=mid+1;
-
-    }
-   if (f[mid]==a)
-  {
     
-    printf("l'element se trouve parmi les resultat");
-  }
-  else if (f[mid]!=a)
-  {
-   printf("l'element est introuvable");
-  }
-  
-   
-  return 0;
-  
- }
+    int r;
+     printf("saisisser votre recherche :");
+     scanf("%d",&r);
 
-    */
+    int start=0;
+    int end=n;
+    int mid=(end+start)/2;
+    while(tab[mid]!=r && start <= end){
+      if (tab[mid]>r)
+         {
+         end=mid-1;
+         mid=(start+end)/2;
+         }
+     else 
+       start=mid+1;
+       mid=(start+end)/2;
+    }
+   if (tab[mid]==r)
+   {
+    printf("l'element se trouve parmi les resultat");
+   }
+    else printf("l'element est introuvable"); 
+   
+   return 0;     
+ }
+ 
+ // fin du programme
+ */
   return 0;
 }
